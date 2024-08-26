f3cd696853eb4a400e6e501b3505a536d5afcb02
sivaselvam31 <sivams3120@gmail.com>
2024-08-26 16:10:05 +0530

    first commit

---
+++ backend/crud/api/views.py
@@ -0,0 +1,39 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializer import BookSerializer 
+
+@api_view(['GET'])
+def get_books(request):
+    books = Book.objects.all()
+    serializedData = BookSerializer(books, many=True).data
+    return Response(serializedData)
+
+@api_view(['POST'])
+def create_books(request):
+    data = request.data
+    serializer = BookSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def book_detail(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'DELETE':
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    
--- froent_end/app/src/App.jsx
+++ froent_end/app/src/App.jsx
@@ -1,19 +1,106 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [release_year, setReleaseYear] = useState(0);
+  const [newTitle, setNewTitle] = useState("");
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
+      const data = await response.json();
+      setBooks(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addBook = async () => {
+    const bookData = {
+      title,
+      release_year,
+    };
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/create/", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bookData),
+      });
+
+      const data = await response.json();
+      setBooks((prev) => [...prev, data]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const updateTitle = async (pk, release_year) => {
+    const bookData = {
+      title: newTitle,
+      release_year,
+    };
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bookData),
+      });
+
+      const data = await response.json();
+      setBooks((prev) =>
+        prev.map((book) => {
+          if (book.id === pk) {
+            return data;
+          } else {
+            return book;
+          }
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteBook = async (pk) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+        method: "DELETE",     
+      });
+      setBooks((prev) => prev.filter((book) => book.id !== pk))
+    }catch (err) {
+      console.log(err)
+    };
+  }
 
   return (
     <>
-     <h1>Book Website</h1>
-     <form>
-      <input type="text" placeholder='Book Tittle' />
-      <input type="number"  placeholder='Release Date' />
-      <button>Add book</button>
-     </form>
+      <h1>Book Website</h1>
+      <form>
+        <input type="text" placeholder="Book Title" onChange={(e) => setTitle(e.target.value)} />
+        <input type="number" placeholder="Release year" onChange={(e) => setReleaseYear(parseInt(e.target.value))} />
+        <button type="button" onClick={addBook}>Add book</button>
+      </form>
+      {books.map((book) => (
+        <div key={book.id}>
+          <h3>Title: {book.title}</h3>
+          <h3>Release Year: {book.release_year}</h3>
+          <input type="text" placeholder="New Title" onChange={(e) => setNewTitle(e.target.value)} />
+          <button type="button" onClick={() => updateTitle(book.id, book.release_year)}>Change Title</button>
+          <button type='button' onClick={() => deleteBook(book.id)}>Delete</button>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
